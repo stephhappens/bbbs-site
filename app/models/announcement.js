6594055f08e1fba008a2a49852c60b0405bbfacd
Kara Fulgum <karafulgum@gmail.com>
2016-12-02 13:04:11 -0600

    Generated announcement model

---
+++ app/models/announcement.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  user: DS.belongsTo('user'),
+  date: DS.attr('date'),
+  headline: DS.attr('string'),
+  message: DS.attr('text')
+});
