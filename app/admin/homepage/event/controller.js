5a9fe2b48bce9ec46931a4f97ba72b3700947fd5
Kara Fulgum <karafulgum@gmail.com>
2016-12-07 11:06:38 -0600

    Loop through real data

--- app/admin/homepage/event/controller.js
+++ app/admin/homepage/event/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     saveForm(formValues) {
-      const event = this.store.createRecord('event');
+      const event = this.store.createRecord('event', formValues);
 
       event.save()
       .then(() => {
