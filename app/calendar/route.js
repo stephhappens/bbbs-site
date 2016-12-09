1f75407f891ef97b61c2771ca37aeccda6ce06c2
stephhappens <stephanie.n.provence@gmail.com>
2016-12-09 16:21:55 -0600

    delete succces alert

--- app/calendar/route.js
+++ app/calendar/route.js
@@ -4,17 +4,17 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('event');
   },
-  afterModel() {
-    this.store.queryRecord('announcement', {
-      latest: true,
-    })
-    .then((latestAnnouncement) => {
-      console.log(latestAnnouncement.get('message'));
-      this.get('flashMessages').success(latestAnnouncement.get('message'), {
-        timeout: 60 * 1000,
-        sticky: true,
-        destroyOnClick: true,
-      });
-    });
-  }
+  // afterModel() {
+  //   this.store.queryRecord('announcement', {
+  //     latest: true,
+  //   })
+  //   .then((latestAnnouncement) => {
+  //     console.log(latestAnnouncement.get('message'));
+  //     this.get('flashMessages').success(latestAnnouncement.get('message'), {
+  //       timeout: 60 * 1000,
+  //       sticky: true,
+  //       destroyOnClick: true,
+  //     });
+  //   });
+  // }
 });
