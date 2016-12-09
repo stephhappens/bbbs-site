c37140b3ab6a09da71a7739428e935d520a202c4
Kara Fulgum <karafulgum@gmail.com>
2016-12-09 13:25:18 -0600

    Install ember power select

--- app/calendar/detail/controller.js
+++ app/calendar/detail/controller.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  tickets: [1, 2, 3, 4],
+  initial: 'Select Amount',
 
 
   actions: {
     saveForm(formValues) {
       this.model.setProperties(formValues);
       this.model.save()
-      .then(() => {
-      });
+        .then(() => {});
 
       return this.transitionToRoute('calendar');
     },
-},
+
+    chooseAmount(tickets) {
+      this.set('initial', tickets);
+    }
+  }
 });
