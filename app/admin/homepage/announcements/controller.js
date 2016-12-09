af4325c69d61082073138bf7b1be5bf13e5c1981
stephhappens <stephanie.n.provence@gmail.com>
2016-12-09 14:38:08 -0600

    create flash message to display on admin

--- app/admin/homepage/announcements/controller.js
+++ app/admin/homepage/announcements/controller.js
@@ -3,9 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
   filesystem: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
 
   actions: {
-      saveMessage(formValues) {
+    saveMessage(formValues) {
+      const flashMessages = this.get('flashMessages');
+      const announcement = this.store.createRecord('announcement', formValues);
+
+      announcement.save()
+        .then(() => {
+          flashMessages.success('Successfully Posted!');
+        })
+        .catch(() => {
+          flashMessages.danger('Something went wrong!');
+        });
+    },
   },
-},
 });
