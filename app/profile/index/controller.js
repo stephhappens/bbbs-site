704726aa55862c8a5ad044b5c122207d621714fc
stephhappens <stephanie.n.provence@gmail.com>
2016-12-13 18:02:28 -0600

    begin message board feature

--- app/profile/index/controller.js
+++ app/profile/index/controller.js
@@ -3,5 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentUser: Ember.inject.service(),
   session: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
 
+  actions: {
+    saveMessage(formValues) {
+      debugger;
+      const flashMessages = this.get('flashMessages');
+      const messages = this.store.createRecord('message', formValues);
+
+      messages.save()
+            .then(() => {
+              flashMessages.success('Successfully Posted!');
+            })
+            .catch(() => {
+              flashMessages.danger('Something went wrong!');
+            });
+    },
+  },
 });
