edc2bdc7f718a6e9b4f2c9a3e4a8f960b224e2a5
Kara Fulgum <karafulgum@gmail.com>
2016-12-15 10:22:23 -0600

    Message time and date

--- app/models/message.js
+++ app/models/message.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   message: DS.attr(),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+  date: DS.attr('string'),
+  time: DS.attr('string'),
 });
