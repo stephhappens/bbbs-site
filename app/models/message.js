e16a966abac672f966aa68126521e5437a57a10d
Kara Fulgum <karafulgum@gmail.com>
2016-12-15 11:40:52 -0600

    Added date and time to message and ordered by newest

--- app/models/message.js
+++ app/models/message.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   message: DS.attr(),
   user: DS.belongsTo('user'),
-  date: DS.attr('string'),
-  time: DS.attr('string'),
+  createdAt: DS.attr('string'),
 });
