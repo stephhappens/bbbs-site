01d7beb49d270fc2187f097f16be9d0de62fa034
stephhappens <stephanie.n.provence@gmail.com>
2016-12-15 16:53:39 -0600

    style admin error message, creating admin access

--- app/admin/route.js
+++ app/admin/route.js
@@ -2,18 +2,18 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  currentUser: Ember.inject.service(),
-  flashMessages: Ember.inject.service(),
-
-  model() {
-    return this.get('currentUser').loadCurrentUser();
-  },
-
-  afterModel(model) {
-    if (!model.get('isAdmin')) {
-      this.get('flashMessages').warning('You are not an admin');
-
-      this.transitionTo('profile');
-    }
-  }
+  // currentUser: Ember.inject.service(),
+  // flashMessages: Ember.inject.service(),
+  //
+  // model() {
+  //   return this.get('currentUser').loadCurrentUser();
+  // },
+  //
+  // afterModel(model) {
+  //   if (!model.get('isAdmin')) {
+  //     this.get('flashMessages').warning('You are not an admin');
+  //
+  //     this.transitionTo('profile');
+  //   }
+  // }
 });
